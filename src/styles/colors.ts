f9221630291d1c4d6b8624a62218c5bda8f2422c
Kalaiyarasan <87932746+Kalai2773@users.noreply.github.com>
2023-07-24 00:47:07 +0530

    Update colors.ts

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,26 +1,26 @@
 import type { Color } from "@/types";
 
 export const primaryColor: Color = {
-   default:"#38bdf8",
+  default: "#0ea5e9",
   _dark: "#38bdf8",
 };
 
 export const secondaryColor: Color = {
-  default: "#c5cadf",
+  default: "#9d9d9d",
   _dark: "#c5cadf",
 };
 
 export const backgroundColor: Color = {
-  default: "#f0e1b31",
+  default: "#fefefe",
   _dark: "#0e1b31",
 };
 
 export const textColor: Color = {
-  default: "#ffffff",
+  default: "#000000",
   _dark: "#ffffff",
 };
 
 export const cardBackgroundColor: Color = {
-  default: "#132347",
+  default: "#ffffff",
   _dark: "#132347",
 };

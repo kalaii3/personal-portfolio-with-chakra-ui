1c14afd023c750d11cf16459a0cedd2a6ad91c56
Kalaiyarasan <87932746+Kalai2773@users.noreply.github.com>
2023-07-30 16:47:21 +0530

    Update colors.ts change

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,25 +2,25 @@ import type { Color } from "@/types";
 
 export const primaryColor: Color = {
   default: "#38bdf8",
-  _dark: "#38bdf8",
+  _dark: "#0ea5e9",
 };
 
 export const secondaryColor: Color = {
-  default: "#c5adf",
-  _dark: "#c5cadf",
+  default: "#c5cadf",
+  _dark: "#9d9d9d",
 };
 
 export const backgroundColor: Color = {
   default: "#0e1b31",
-  _dark: "#0e1b31",
+  _dark: "#fefefe",
 };
 
 export const textColor: Color = {
   default: "#ffffff",
-  _dark: "#fffffff",
+  _dark: "#000000",
 };
 
 export const cardBackgroundColor: Color = {
   default: "#132347",
-  _dark: "#132347",
+  _dark: "#ffffff",
 };

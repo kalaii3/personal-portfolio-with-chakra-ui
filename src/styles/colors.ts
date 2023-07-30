5ae78d2499ff00a2d474af6d42ce3c3faae436ae
Kalaiyarasan <87932746+Kalai2773@users.noreply.github.com>
2023-07-30 15:13:35 +0530

    color changes #err

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,26 +1,27 @@
 import type { Color } from "@/types";
 
 export const primaryColor: Color = {
-  default: "#0ea5e9",
+  default: "##38bdf8",
   _dark: "#38bdf8",
 };
 
 export const secondaryColor: Color = {
-  default: "#9d9d9d",
+  default: "#c5cadf",
   _dark: "#c5cadf",
 };
 
 export const backgroundColor: Color = {
-  default: "#fefefe",
+  default: "#0e1b31",
   _dark: "#0e1b31",
 };
 
 export const textColor: Color = {
-  default: "#000000",
+  default: "#ffffff",
   _dark: "#ffffff",
 };
 
 export const cardBackgroundColor: Color = {
-  default: "#ffffff",
+  default: "##132347",
   _dark: "#132347",
 };
+

da86bc31965466be0a21d9cffa7532de119cc67a
Kalaiyarasan <87932746+Kalai2773@users.noreply.github.com>
2023-08-05 12:08:33 +0530

    changed typewriting text

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -9,4 +9,19 @@ export const profileData: Profile = {
     "I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️I am fluent in classics like Python , Javascript and PHP.My field of Interest's are building new Web Technologies and Products.",
     "I am a junior pursuing B.Tech Information Technology in Mookambigai College of Engineering ,Pudukkottai. I am a Full Stack Developer who is passionate about creating beautiful and joyful digital experiences. Besides development, I love music, games and travelling.",
   ],
+
+  experiences: [
+    {
+      number: 2,
+      title: "Years Of Experience",
+    },
+    {
+      number: 4,
+      title: "Programming Language Used",
+    },
+    {
+      number: 6,
+      title: "Successed Projects",
+    },
+  ],
 };

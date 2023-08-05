2ee8ff69429d9acc181b92052ab9e6c4fc616cda
Kalaiyarasan <87932746+Kalai2773@users.noreply.github.com>
2023-08-05 12:05:53 +0530

    Update profile.ts

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -3,24 +3,10 @@ import type { Profile } from "@/types";
 export const profileData: Profile = {
   name: "Kalai",
   region: "India",
-  divisions: ["Frontend Developer", "Engineering Student"],
+  divisions: ["Full Stack Developer", "Engineering Student"],
   pictures: ["/assets/svg/kalai.svg", "/assets/images/Horizontal.png"],
   descriptions: [
     "I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️I am fluent in classics like Python , Javascript and PHP.My field of Interest's are building new Web Technologies and Products.",
     "I am a junior pursuing B.Tech Information Technology in Mookambigai College of Engineering ,Pudukkottai. I am a Full Stack Developer who is passionate about creating beautiful and joyful digital experiences. Besides development, I love music, games and travelling.",
-  ],
-  experiences: [
-    {
-      number: 2,
-      title: "Years Of Experience",
-    },
-    {
-      number: 4,
-      title: "Programming Language Used",
-    },
-    {
-      number: 6,
-      title: "Successed Projects",
-    },
   ],
 };

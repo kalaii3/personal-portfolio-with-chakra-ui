b60f4e6976922a773a926171cee2d55e6240af78
Kalaiyarasan <87932746+kalaii3@users.noreply.github.com>
2025-07-21 15:42:38 +0530

    Update profile.ts

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -7,8 +7,7 @@ export const profileData: Profile = {
   pictures: ["/assets/svg/kalai.svg", "/assets/images/Horizontal.png"],
   descriptions: [
     "I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️I am fluent in classics like Python , Javascript and PHP.My field of Interest's are building new Web Technologies and Products.",
-    "Frontend developer, building responsive and interactive web applications. Proficient in modern web technologies including HTML5, CSS3, JavaScript (ES6+), and React.js.
-",
+    "Frontend developer, building responsive and interactive web applications. Proficient in modern web technologies including HTML5, CSS3, JavaScript (ES6+), and React.js.",
   ],
 
   experiences: [
